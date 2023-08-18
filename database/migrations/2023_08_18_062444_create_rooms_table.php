4fcfb627abf10259d4738b9e27e547f9526fd407
Nikki Zapuiz <ma.nikki.zapuiz@gmail.com>
2023-08-18 16:44:34 +0800

    card_file_box: Created database  migration files

--- database/migrations/2023_08_18_062444_create_rooms_table.php
+++ database/migrations/2023_08_18_062444_create_rooms_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->string('full_name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['admin', 'guest']);
-            $table->rememberToken();
+            $table->integer('room_number')->nullable();
+            $table->enum('room_type', ['standard', 'deluxe', 'suite']);
+            $table->boolean('is_available');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rooms');
     }
 };
---
+++ database/migrations/2023_08_18_063503_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('reservation_number')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('room_id')->constrained();
+            $table->timestamp('checkin_date')->nullable();
+            $table->timestamp('checkout_date')->nullable();
+            $table->integer('ault_count')->nullable();
+            $table->integer('child_count')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
